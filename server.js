1134910cc2375f937928f8eea89c6faf4130c610
dongbaram <dongbaram@naver.com>
2019-05-08 10:33:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
+
+var multer  =   require('multer');  //파일 업로드 //ocr-----------------
     
 Object.assign=require('object-assign')
 
@@ -114,6 +116,61 @@ app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
+
+//ocr-----------------------------------------------------------------------------------
+app.post('/fileUpload',function(req,res) {
+    var storage =   multer.diskStorage({
+        
+        destination: function (req, file, cb) {                 // 파일 업로드 경로 지정
+          cb(null, './uploads');
+        },
+        
+        filename: function (req, file, cb) {                    //파일 업로드 처리
+          console.log("file.originalname:"+file.originalname);
+ 
+          upfilename = Date.now()+"_"+file.originalname         //날짜 + 시간 + 원래 파일명
+          cb(null, upfilename );
+          console.log("upfilename:"+upfilename);
+
+        }
+      }); 
+
+    var upload = multer({ storage : storage}).single('uploadFile'); //client 에서 호출명
+
+    console.log("app post /fileUpload"); 
+
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }else{
+            console.log(req.body);      //client 에게  받은 파라미터
+            console.log("key1:"+req.body.key1);
+        }
+
+        //파이썬 호출----------------------------------
+        var spawn = require('child_process').spawn,
+        py = spawn('python',['./python/nodejs_call_data.py']),  //파이썬 호출 파일
+        data = {"param1":"v1","param2":"v2"},       //파이썬에 전달할 파라미터
+        dataString = "";
+        py.stdout.on('data',function(data){
+            dataString += data.toString();
+        });
+        py.stdout.on('end',function(){
+            console.log('결과:'+dataString);
+        });
+        py.stdin.write(JSON.stringify(data));       //파이썬 실행
+        py.stdin.end();
+        //--------------------------------------------------
+
+        //결과 리턴 -----------------------------------------
+        res.writeHead(200,{"content-Type":"text/html; charset=utf-8"})
+        //res.write("File is uploaded:",res.filename)
+        res.write("File is uploaded:",dataString)
+        res.end();
+    }); 
+
+});
+//ocr--end---------------------------------------------------------------------------------
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
